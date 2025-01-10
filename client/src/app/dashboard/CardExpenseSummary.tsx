c49e2c1d795a33a1076ff533590f92261e25ddee
Esteban Medina Palacio <estebanmedina9@gmail.com>
2025-01-09 22:18:00 -0500

    Add pie chart data

--- client/src/app/dashboard/CardExpenseSummary.tsx
+++ client/src/app/dashboard/CardExpenseSummary.tsx
@@ -1,11 +1,33 @@
 import { useGetDashboardMetricsQuery } from "@/state/api";
+import { ExpenseByCategorySummary } from "@/utils/interfaces/interfaces";
 import React from "react";
-import { Pie, PieChart, ResponsiveContainer } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 const colors = ["#00C49F", "#0088FE", "#FFBB28"];
-
+type ExpenseSums = {
+  [category: string]: number;
+};
 const CardExpenseSummary = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+  const expenseByCategorySummary =
+    dashboardMetrics?.expenseByCategorySummary || [];
+  const expenseSums = expenseByCategorySummary.reduce(
+    (accumulation: ExpenseSums, item: ExpenseByCategorySummary) => {
+      const category = item.category + " Expenses";
+      const amount = parseInt(item.amount, 10);
+      if (!accumulation[category]) accumulation[category] = 0;
+      accumulation[category] += amount;
+      return accumulation;
+    },
+    {}
+  );
+
+  const expenseCategories = Object.entries(expenseSums).map(
+    ([name, value]) => ({
+      name,
+      value,
+    })
+  );
   return (
     <div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
       {isLoading ? (
@@ -23,11 +45,28 @@ const CardExpenseSummary = () => {
           <div className="xl:flex justify-between pr-7">
             {/* CHART */}
             <div className="relative basis-3/5">
-                <ResponsiveContainer width="100%" height={140}>
-                    <PieChart>
-                        <Pie data={expenseCategories} innerRadius={50} outerRadius={60} fill="#8884d8" dataKey="value" nameKey="name" cx="50%" cy="50%"></Pie>
-                    </PieChart>
-                </ResponsiveContainer>
+              <ResponsiveContainer width="100%" height={140}>
+                <PieChart>
+                  <Pie
+                    data={expenseCategories}
+                    innerRadius={50}
+                    outerRadius={60}
+                    fill="#8884d8"
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                  >
+
+                    {expenseCategories.map((entry, index)=>(
+                      <Cell
+                        key={`cel-${index}`}
+                        fill={colors[index % colors.length]}
+                        />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
             </div>
           </div>
         </>

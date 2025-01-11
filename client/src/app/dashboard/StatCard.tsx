3e16a94e686cabab5f1f4958e2a841447dd573c8
Esteban Medina Palacio <estebanmedina9@gmail.com>
2025-01-10 22:45:50 -0500

    feat: Create StatCard file

---
+++ client/src/app/dashboard/StatCard.tsx
@@ -0,0 +1,27 @@
+import { LucideIcon } from 'lucide-react';
+import React, { JSX } from 'react'
+
+
+type StatDetail = {
+    title: string;
+    amount: string;
+    changePercentage: number;
+    IconComponent: LucideIcon
+}
+type StatCardProps = {
+    title: string;
+    primaryIcon: JSX.Element;
+    details: StatDetail[];
+    dateRange: string
+}
+
+const StatCard = ({title,
+    primaryIcon,
+    details,
+    dateRange,}: StatCardProps) => {
+  return (
+    <div>StatCard</div>
+  )
+}
+
+export default StatCard

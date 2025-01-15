062f3b2fa0dc0552589e561b5a954bd2e8356263
Esteban Medina Palacio <estebanmedina9@gmail.com>
2025-01-14 21:12:41 -0500

    feat: Add DataGrid

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -2,10 +2,34 @@
 import { useGetProductsQuery } from "@/state/api";
 import React from "react";
 import Header from "../(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery;
-
+  const columns: GridColDef[] = [
+    { field: "productId", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Product Name", width: 200 },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 110,
+      type: "number",
+      valueGetter: (vale, row) => `${row.price}`,
+    },
+    {
+      field: "rating",
+      headerName: "Rating",
+      width: 110,
+      type: "number",
+      valueGetter: (vale, row) => (row.rating ? row.rating : "N/A"),
+    },
+    {
+      field: "stockQuantity",
+      headerName: "Stock Quantity",
+      width: 150,
+      type: "number",
+    },
+  ];
   if (isLoading) {
     return <div className="py-4">Loading...</div>;
   }
@@ -17,9 +41,18 @@ const Inventory = () => {
     );
   }
 
-  return <div className="flex flex-col">
-    <Header name="Inventory" />
-  </div>
+  return (
+    <div className="flex flex-col">
+      <Header name="Inventory" />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        getRowId={(row) => row.productId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+      />
+    </div>
+  );
 };
 
 export default Inventory;

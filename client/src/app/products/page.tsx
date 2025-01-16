f0b52bf32e02825ee233ed464b6e86ece469c170
Esteban Medina Palacio <estebanmedina9@gmail.com>
2025-01-15 22:27:35 -0500

    feat: Create Prdoucts page

---
+++ client/src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function products() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState("");
+
+  return <div>products</div>;
+}
+
+export default products;
